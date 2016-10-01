7e52c979ef6244e00b489f0ca24132db5dbe8118
Ruochi Zhang <lvduzhen@gmail.com>
2016-10-01 20:44:10 +0800

    2016.10.01

--- pickle_.py
+++ pickle_.py
@@ -7,16 +7,6 @@ class GameState:
         self.lives = lives
         self.points = points
 
-def pickle_game_state(game_state):
-    kwargs = game_state.__dict__
-    return unpickle_game_state, (kwargs,)     
-
-def unpickle_game_state(kwargs):
-    return GameState(**kwargs)
-
-
-copyreg.pickle(GameState,pickle_game_state)
-
 
 state = GameState()
 state.points += 100
@@ -29,12 +19,26 @@ state.__setattr__("magic",5)
 print(state.__dict__)
 """
 
-class GameState:
-    def __init__(self,level = 0,lives = 4,points = 0,magic = 5):
+#修改类名 增加和删除属性等都可以用 copyreg 模块解决
+class BetterGameState:
+    def __init__(self,level = 0,points = 0,magic = 5):
         self.level = level
-        self.lives = lives
         self.points = points
         self.magic = magic
+
+def pickle_game_state(game_state):
+    kwargs = game_state.__dict__
+    kwargs["version"] = 2
+    return unpickle_game_state, (kwargs,)     
+
+def unpickle_game_state(kwargs):
+    version = kwargs.pop("version",1)
+    if version == 1:
+        kwargs.pop("lives")
+    return GameState(**kwargs)
+
+
+copyreg.pickle(BetterGameState,pickle_game_state)        
 
 
 state_after_2 = pickle.loads(serialized)
