cbc8c831d43aaded60e31e809e62922cada70ad1
zhangruochi <zrc720@gmail.com>
2017-07-23 10:29:44 +0800

    2017-07-23

---
+++ test.py
@@ -0,0 +1,6 @@
+import os
+import time
+print('\n'.join([''.join([('XIAOYUE!'[(x-y)%7]if((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3<=0 else' ')for x in range(-30,30)])for y in range(15,-15,-1)]))
+
+time.sleep(5)
+os.system("say 'hello world!'")
